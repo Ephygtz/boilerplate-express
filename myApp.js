8de34fcd06bbc9fafe1996369842af64561569fe
Ephra <ephygtz@gmail.com>
2024-02-09 22:33:46 +0300

    using .env file n12

--- myApp.js
+++ myApp.js
@@ -8,14 +8,19 @@ app.get("/", function(req, res){
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", function(req, res){
-    // let caseN = process.env.MESSAGE_STYLE;
-    if(process.env.MESSAGE_STYLE === "uppercase"){
-        res.json( {"message": "Hello json".toUpperCase()});
-    } else {
-        res.json( {"message": "Hello json"});
-    }
-});
+app.get(’/json’, (req, res) => {
+    let message = ‘Hello json’;
+    (process.env.MESSAGE_STYLE == ‘uppercase’) ? message=message.toUpperCase() : message=message; res.json({‘message’: message});
+    });
+
+// app.get("/json", function(req, res){
+//     // let caseN = process.env.MESSAGE_STYLE;
+//     if(process.env.MESSAGE_STYLE === "uppercase"){
+//         res.json( {"message": "Hello json".toUpperCase()});
+//     } else {
+//         res.json( {"message": "Hello json"});
+//     }
+// });
 
 
 
